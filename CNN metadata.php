f2d3cab409aee42471e65a38aa4a5d77c8c1aea9
Laveena Jethani <laveena.j.21@gmail.com>
2021-07-10 00:05:56 +0530

    updated code js with php

--- CNN metadata.php
+++ CNN metadata.php
@@ -1,29 +1,24 @@
-<?php
-	
-if(isset($_REQUEST["btn_Upload"]))
-{
-	echo $_FILES["FU_Photo"]["name"]."<br>";
-	echo $_FILES["FU_Photo"]["size"]."<br>";
-	echo $_FILES["FU_Photo"]["type"]."<br>";
-	echo $_FILES["FU_Photo"]["tmp_name"]."<br>";
-	
-	$fn=$_FILES["FU_Photo"]["name"];
-	$spath=$_FILES["FU_Photo"]["tmp_name"];
-	$dpath="Cars/".$fn;
-	
-	move_uploaded_file($spath, $dpath);
-	
-	
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="CNN metadata.css" />
     <title>Vehicle Information</title>
+    <script type="text/javascript">
+    	    function myfun() {
+    	        var x = `"<?php
+                if(isset($_REQUEST["btn_Upload"]))
+				{	
+					$fn=$_FILES["FU_Photo"]["name"];
+					$spath=$_FILES["FU_Photo"]["tmp_name"];
+					$dpath="Cars/".$fn;
+					move_uploaded_file($spath, $dpath);
+					$last_line = system('python main.py', $retval);
+				}?>"` 	
+				document.getElementById("out").innerHTML = x;
+			
+             }
+    </script>
 </head>
 <body>
 	<form enctype="multipart/form-data" method="post">
@@ -34,11 +29,11 @@ if(isset($_REQUEST["btn_Upload"]))
     <h4><label for="file">Select Image: </label>
     <input type="file" id="file" name="FU_Photo"  style="font-size: 15px;"/><br>
     </h4>
-    <input type="submit" value="Upload" name="btn_Upload">
+    <input type="submit" value="Upload" name="btn_Upload" onclick="myfun()">
     <center>
-  </div>
-   </form>
-  
+    </div>
+    </form>
+    <div id="out">output</div>
 </body>
 </html>
 
